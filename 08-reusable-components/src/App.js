ea53d6dc50f581744422ab054afe7ac4dc985208
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-05-02 17:22:52 +0300

    08-2

--- 08-reusable-components/src/App.js
+++ 08-reusable-components/src/App.js
@@ -1,7 +1,22 @@
 import './App.css';
 
+function MyComponent() {
+   return (
+      <div>
+         <h1>Hello from the reusable component</h1>
+         <button>Like!</button>
+      </div>
+   );
+}
+
 function App() {
-   return <div className="App"></div>;
+   return (
+      <div className="App">
+         <MyComponent />
+         <MyComponent />
+         <MyComponent />
+      </div>
+   );
 }
 
 export default App;
