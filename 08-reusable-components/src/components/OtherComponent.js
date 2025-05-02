b814af6973b04c0a749ff6a782be2f8bff04f617
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-05-02 22:03:42 +0300

    09

--- 08-reusable-components/src/components/OtherComponent.js
+++ 08-reusable-components/src/components/OtherComponent.js
@@ -1,12 +1,7 @@
 function OtherComponent() {
    return (
       <div>
-         <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo autem
-            fugit quaerat tempora fuga, provident eligendi voluptatum. Veritatis
-            corrupti facere praesentium commodi sint! Necessitatibus magnam odit
-            debitis, minima maxime reiciendis.
-         </p>
+         <p>Hello from React</p>
       </div>
    );
 }
