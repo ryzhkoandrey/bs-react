a84d95d0676ee236e225a4fa900626d3150edf66
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-05-03 03:18:15 +0300

    11-3

--- 11-state/src/components/RandomNumber.js
+++ 11-state/src/components/RandomNumber.js
@@ -1,7 +1,15 @@
+import { useState } from 'react';
+
+function generateRandomNum() {
+   return Math.floor(Math.random() * 100);
+}
+
 function RandomNumber() {
+   const [randomNum, setRandomNum] = useState(generateRandomNum());
+
    return (
       <div>
-         <h1>100</h1>
+         <h1>{randomNum}</h1>
          <button>Generate new random number</button>
       </div>
    );
