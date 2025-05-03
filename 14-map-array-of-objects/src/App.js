867857494ade07e2c56a05dfeaeba57e48b05720
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-05-04 02:26:41 +0300

    14-2

--- 14-map-array-of-objects/src/App.js
+++ 14-map-array-of-objects/src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
+import persons from './data/persons';
 
 function App() {
-   return <div className="App"></div>;
+   return <div className='App'></div>;
 }
 
 export default App;
