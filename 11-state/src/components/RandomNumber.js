4a07f81bba85e996bef2ce774eda8db52dc55cc3
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-05-03 03:51:05 +0300

    11-4

--- 11-state/src/components/RandomNumber.js
+++ 11-state/src/components/RandomNumber.js
@@ -7,10 +7,14 @@ function generateRandomNum() {
 function RandomNumber() {
    const [randomNum, setRandomNum] = useState(generateRandomNum());
 
+   const changeRandomNum = () => {
+      setRandomNum(generateRandomNum());
+   };
+
    return (
       <div>
          <h1>{randomNum}</h1>
-         <button>Generate new random number</button>
+         <button onClick={changeRandomNum}>Generate new random number</button>
       </div>
    );
 }
