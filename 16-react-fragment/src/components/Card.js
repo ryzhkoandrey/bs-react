e51d49057c0ac95b4b36643ede7e35d3aa90c50e
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-05-04 22:43:29 +0300

    16

---
+++ 16-react-fragment/src/components/Card.js
@@ -0,0 +1,11 @@
+const Card = () => {
+   return (
+      <div>
+         <h1>Bogdan</h1>
+         <h3>I am recording React Course</h3>
+         <button>Like!</button>
+      </div>
+   );
+};
+
+export default Card;
