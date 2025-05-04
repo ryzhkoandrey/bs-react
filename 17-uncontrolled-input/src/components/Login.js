f3f9c7030d33068fdd278648b5301d42e9ad1647
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-05-05 01:36:34 +0300

    17

---
+++ 17-uncontrolled-input/src/components/Login.js
@@ -0,0 +1,23 @@
+function Login() {
+   return (
+      <>
+         <h1>Login form</h1>
+
+         <form>
+            <label>
+               Username:
+               <input type="text" name="username" />
+            </label>
+
+            <label>
+               Password:
+               <input type="password" name="password" />
+            </label>
+
+            <button type="submit">Login</button>
+         </form>
+      </>
+   );
+}
+
+export default Login;
