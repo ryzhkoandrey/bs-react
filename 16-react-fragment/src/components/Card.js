738deafd8725f94cc1ee7964cf16dd1dab662e30
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-05-05 00:00:30 +0300

    16-2

--- 16-react-fragment/src/components/Card.js
+++ 16-react-fragment/src/components/Card.js
@@ -1,10 +1,10 @@
 const Card = () => {
    return (
-      <div>
+      <>
          <h1>Bogdan</h1>
          <h3>I am recording React Course</h3>
          <button>Like!</button>
-      </div>
+      </>
    );
 };
 
