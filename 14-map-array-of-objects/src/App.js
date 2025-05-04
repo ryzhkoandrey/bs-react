51313165eba4d33441bc276579b196b1149e8bc6
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-05-04 05:42:28 +0300

    14-5

--- 14-map-array-of-objects/src/App.js
+++ 14-map-array-of-objects/src/App.js
@@ -6,16 +6,7 @@ function App() {
    return (
       <div className='App'>
          {persons.map((person) => {
-            return (
-               <Person
-                  key={person.id}
-                  id={person.id}
-                  firstName={person.firstName}
-                  lastName={person.lastName}
-                  email={person.email}
-                  img={person.img}
-               />
-            );
+            return <Person key={person.id} {...person} />;
          })}
       </div>
    );
