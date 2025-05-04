ea538c34329f7d333b596f2a57a3e916fb25e389
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-05-04 06:00:26 +0300

    14-6

--- 14-map-array-of-objects/src/components/Person.js
+++ 14-map-array-of-objects/src/components/Person.js
@@ -1,5 +1,11 @@
-function Person({ id, firstName, lastName, email, img }) {
-   return <div></div>;
+function Person({ firstName, lastName, email, img }) {
+   return (
+      <div>
+         <img src={img} />
+         <h3>{`${firstName} ${lastName}`}</h3>
+         <h4>{email}</h4>
+      </div>
+   );
 }
 
 export default Person;
