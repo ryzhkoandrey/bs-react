0f6a92a7716d063e055372277b6bc20f06ee683e
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-05-05 06:48:43 +0300

    19

---
+++ 19-children-prop/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Wrapper from './components/Wrapper';
+
+function App() {
+   return (
+      <div className="App">
+         <Wrapper>
+            <h2>Text inside of the Wrapper</h2>
+            <button>Click Me</button>
+         </Wrapper>
+      </div>
+   );
+}
+
+export default App;
