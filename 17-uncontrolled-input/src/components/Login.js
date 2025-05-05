a5946e7878f9c5e053334eff602a013985197de9
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-05-05 03:18:24 +0300

    17-2

--- 17-uncontrolled-input/src/components/Login.js
+++ 17-uncontrolled-input/src/components/Login.js
@@ -1,9 +1,21 @@
 function Login() {
+   function handleFormSubmit(event) {
+      event.preventDefault();
+
+      const userData = {
+         username: event.target.username.value,
+         password: event.target.password.value,
+      };
+
+      console.log(userData);
+      alert(JSON.stringify(userData));
+   }
+
    return (
       <>
          <h1>Login form</h1>
 
-         <form>
+         <form onSubmit={handleFormSubmit}>
             <label>
                Username:
                <input type="text" name="username" />
