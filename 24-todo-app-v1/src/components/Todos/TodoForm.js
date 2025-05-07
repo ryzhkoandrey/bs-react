4e1f92178655f478784f9297baf7d525ce8a9add
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-05-07 04:48:08 +0300

    24-2

---
+++ 24-todo-app-v1/src/components/Todos/TodoForm.js
@@ -0,0 +1,10 @@
+function TodoForm() {
+   return (
+      <form>
+         <input type="text" placeholder="Enter new Todo" />
+         <button type="submit">Submit</button>
+      </form>
+   );
+}
+
+export default TodoForm;
