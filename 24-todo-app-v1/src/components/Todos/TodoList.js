62893c1041be7b46c060d7fd74f843170227d028
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-05-07 09:36:13 +0300

    24-7

--- 24-todo-app-v1/src/components/Todos/TodoList.js
+++ 24-todo-app-v1/src/components/Todos/TodoList.js
@@ -1,12 +1,13 @@
 import Todo from './Todo';
+import styles from './TodoList.module.css';
 
 function TodoList({ todos }) {
    return (
-      <>
+      <div className={styles.todoListContainer}>
          {todos.map((todo, index) => (
             <Todo key={index} todo={todo} />
          ))}
-      </>
+      </div>
    );
 }
 
