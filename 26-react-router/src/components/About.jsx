bb77050d74a0aa0afe2765deb01ba8573d7796a1
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-05-13 11:42:56 +0300

    26-4

--- 26-react-router/src/components/About.jsx
+++ 26-react-router/src/components/About.jsx
@@ -1,5 +1,14 @@
+import { Link } from 'react-router-dom';
+
 const About = () => {
-   return <h1>About</h1>;
+   return (
+      <>
+         <h1>About</h1>
+         <Link to="/">Go to Home</Link>
+         <br />
+         <Link to="/contacts">Go to Contacts</Link>
+      </>
+   );
 };
 
 export default About;
