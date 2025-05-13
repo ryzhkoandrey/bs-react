49b54fcb298ef94e77b3e29ab80ed1fa4af010ad
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-05-13 15:20:27 +0300

    26-9

--- 26-react-router/src/components/Menu.jsx
+++ 26-react-router/src/components/Menu.jsx
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom';
 const Menu = () => {
    return (
       <nav>
-         <Link to="/">Home</Link>
-         <Link to="/about">About</Link>
-         <Link to="/contacts">Contacts</Link>
+         <Link to=".">Home</Link>
+         <Link to="about">About</Link>
+         <Link to="contacts">Contacts</Link>
       </nav>
    );
 };
