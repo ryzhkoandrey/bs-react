0b2ba1f9575850548eb874f12f1569e9b6905c22
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-05-13 09:27:44 +0300

    26-2

--- 26-react-router/src/App.js
+++ 26-react-router/src/App.js
@@ -1,11 +1,16 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
    return (
       <BrowserRouter>
          <div className="App">
-            <h1>React Router</h1>
+            <Routes>
+               <Route path="*" element={<h1>404</h1>} />
+               <Route path="/" element={<h1>Home</h1>} />
+               <Route path="about" element={<h1>About</h1>} />
+               <Route path="contacts" element={<h1>Contacts</h1>} />
+            </Routes>
          </div>
       </BrowserRouter>
    );
