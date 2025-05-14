8a3bfad7c16e865e2924083aa144bbd2dd8a4ab6
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-05-14 14:43:41 +0300

    26-17

--- 26-react-router/src/components/SingleCourse.jsx
+++ 26-react-router/src/components/SingleCourse.jsx
@@ -1,5 +1,10 @@
+import { useParams } from 'react-router-dom';
+import courses from '../data/courses.js';
+
 const SingleCourse = () => {
-   return <h1>Single Course Info</h1>;
+   const params = useParams();
+
+   return <h1>{params.courseSlug}</h1>;
 };
 
 export default SingleCourse;
