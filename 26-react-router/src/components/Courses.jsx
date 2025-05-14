7b3b4b1a446372624f40d3446e8b9df68af7dc0a
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-05-14 14:18:14 +0300

    26-16

--- 26-react-router/src/components/Courses.jsx
+++ 26-react-router/src/components/Courses.jsx
@@ -1,5 +1,20 @@
+import { Link } from 'react-router-dom';
+import courses from '../data/courses.js';
+
 const Courses = () => {
-   return <h1>Courses</h1>;
+   return (
+      <>
+         <h1>Courses</h1>
+
+         {courses.map((course) => (
+            <div key={course.id}>
+               <Link to={course.slug} className="courseLink">
+                  {course.title}
+               </Link>
+            </div>
+         ))}
+      </>
+   );
 };
 
 export default Courses;
