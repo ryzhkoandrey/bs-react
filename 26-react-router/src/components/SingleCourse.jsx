8dff07682218ea3d10b038a21120d322dd9ea8a5
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-05-15 06:31:11 +0300

    26-19

--- 26-react-router/src/components/SingleCourse.jsx
+++ 26-react-router/src/components/SingleCourse.jsx
@@ -1,15 +1,26 @@
 import { Link, useParams } from 'react-router-dom';
 import courses from '../data/courses.js';
+import NotFound from './NotFound.jsx';
 
 const SingleCourse = () => {
    const params = useParams();
-   const { title, slug, id } = courses.find((course) => course.slug === params.slug);
+   const course = courses.find((course) => course.slug === params.slug);
+
+   if (!course)
+      return (
+         <>
+            <NotFound />
+            <Link to=".." relative="path">
+               All courses
+            </Link>
+         </>
+      );
 
    return (
       <>
-         <h1>{title}</h1>
-         <h2>{slug}</h2>
-         <h3>{id}</h3>
+         <h1>{course.title}</h1>
+         <h2>{course.slug}</h2>
+         <h3>{course.id}</h3>
          <Link to=".." relative="path">
             All courses
          </Link>
