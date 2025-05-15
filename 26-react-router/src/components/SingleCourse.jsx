220ab0928630da681cf423cc3f174c2f2100bd6f
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-05-15 08:32:14 +0300

    26-20

--- 26-react-router/src/components/SingleCourse.jsx
+++ 26-react-router/src/components/SingleCourse.jsx
@@ -1,26 +1,23 @@
-import { Link, useParams } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+import { useEffect } from 'react';
 import courses from '../data/courses.js';
-import NotFound from './NotFound.jsx';
 
 const SingleCourse = () => {
    const params = useParams();
+   const navigate = useNavigate();
    const course = courses.find((course) => course.slug === params.slug);
 
-   if (!course)
-      return (
-         <>
-            <NotFound />
-            <Link to=".." relative="path">
-               All courses
-            </Link>
-         </>
-      );
+   useEffect(() => {
+      if (!course) {
+         navigate('..', { relative: 'path' });
+      }
+   }, [course]);
 
    return (
       <>
-         <h1>{course.title}</h1>
-         <h2>{course.slug}</h2>
-         <h3>{course.id}</h3>
+         <h1>{course?.title}</h1>
+         <h2>{course?.slug}</h2>
+         <h3>{course?.id}</h3>
          <Link to=".." relative="path">
             All courses
          </Link>
