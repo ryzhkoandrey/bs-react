b93225b9aafddae62f11cafa709cd16a43d24c37
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-05-15 17:49:57 +0300

    26-23

--- 26-react-router/src/components/Courses.jsx
+++ 26-react-router/src/components/Courses.jsx
@@ -3,18 +3,22 @@ import { Link, useLocation } from 'react-router-dom';
 import queryString from 'query-string';
 import courses from '../data/courses.js';
 
+function sortCourses(courses, key) {
+   const sortedCourses = [...courses];
+   return sortedCourses.sort((a, b) => (a[key] > b[key] ? 1 : -1));
+}
+
 const Courses = () => {
    const location = useLocation();
    const query = queryString.parse(location.search);
    const [sortKey, setSortKey] = useState(query.sort);
-   console.log(sortKey);
-   const [sortedCourses, setSortedCourses] = useState();
+   const [sortedCourses, setSortedCourses] = useState(sortCourses(courses, sortKey));
 
    return (
       <>
          <h1>Courses</h1>
 
-         {courses.map((course) => (
+         {sortedCourses.map((course) => (
             <div key={course.id}>
                <Link to={course.slug} className="courseLink">
                   {course.title}
