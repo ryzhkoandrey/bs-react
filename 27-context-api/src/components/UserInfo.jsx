461651a52c64146879b0c1fbbe369f2e3e453eb7
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-05-21 18:25:55 +0300

    27-5

--- 27-context-api/src/components/UserInfo.jsx
+++ 27-context-api/src/components/UserInfo.jsx
@@ -1,7 +1,10 @@
+import { useContext } from 'react';
 import UserContext from '../context/UserContext';
 
 function UserInfo() {
-   return <UserContext.Consumer>{(value) => <h1>{value}</h1>}</UserContext.Consumer>;
+   const user = useContext(UserContext);
+
+   return <h1>{user}</h1>;
 }
 
 export default UserInfo;
