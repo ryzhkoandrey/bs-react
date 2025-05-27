a1ef666cc83227d7c9e7ce01f0b618c9d79af332
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-05-27 16:26:35 +0300

    28-3

--- 28-reducer/reducer.js
+++ 28-reducer/reducer.js
@@ -1,14 +1,31 @@
 const initialState = [];
 
-// {
-//     type: 'ADD_NAME',
-//     payload: 'Alice',
-// }
-
 function reducer(state, action) {
    if (action.type === 'ADD_NAME') {
       return [...state, action.payload];
    }
 
+   if (action.type === 'DELETE_NAME') {
+      return state.filter((personName) => personName !== action.payload);
+   }
+
    return state;
 }
+
+let newState = reducer(initialState, {
+   type: 'ADD_NAME',
+   payload: 'Bogdan',
+});
+console.log(newState);
+
+newState = reducer(newState, {
+   type: 'ADD_NAME',
+   payload: 'Alice',
+});
+console.log(newState);
+
+newState = reducer(newState, {
+   type: 'DELETE_NAME',
+   payload: 'Alice',
+});
+console.log(newState);
