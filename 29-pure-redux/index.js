42238b37f1b192b896b10b778d8b66f10b3c3c1e
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-05-28 15:09:20 +0300

    29-8

--- 29-pure-redux/index.js
+++ 29-pure-redux/index.js
@@ -2,6 +2,7 @@ import store from './redux/store.js';
 import getCurrentTime from './utils/getCurrentTime.js';
 
 const addTimeBtn = document.getElementById('addTime');
+const clearTimesBtn = document.getElementById('clearTimes');
 const timesList = document.getElementById('timesList');
 
 addTimeBtn.addEventListener('click', () => {
@@ -11,11 +12,16 @@ addTimeBtn.addEventListener('click', () => {
    });
 });
 
+clearTimesBtn.addEventListener('click', () => {
+   store.dispatch({
+      type: 'CLEAR_ALL_TIMES',
+   });
+});
+
 store.subscribe(() => {
    timesList.innerHTML = '';
 
    const times = store.getState();
-
    times.forEach((time) => {
       let li = document.createElement('li');
       li.innerText = time;
