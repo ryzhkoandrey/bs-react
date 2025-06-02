07603c0af9a6bb394332fc5a84b2b0509bc3a9e3
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-06-02 14:14:12 +0300

    30+

--- 30-react-redux/frontend/src/components/BookList/BookList.js
+++ 30-react-redux/frontend/src/components/BookList/BookList.js
@@ -1,9 +1,26 @@
+import { useSelector } from 'react-redux';
 import './BookList.css';
 
 const BookList = () => {
+   const books = useSelector((state) => state.books);
+
    return (
       <div className="app-block book-list">
          <h2>Book List</h2>
+
+         {books.length === 0 ? (
+            <p>No books available</p>
+         ) : (
+            <ul>
+               {books.map((book, i) => (
+                  <li key={i}>
+                     <div className="book-info">
+                        {book.title} by {book.author}
+                     </div>
+                  </li>
+               ))}
+            </ul>
+         )}
       </div>
    );
 };
