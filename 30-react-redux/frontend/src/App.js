fcdc4b8cf5dec0554dd198b8e012c7c3b6536a5d
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-06-11 18:47:47 +0300

    30+

--- 30-react-redux/frontend/src/App.js
+++ 30-react-redux/frontend/src/App.js
@@ -14,7 +14,6 @@ function App() {
             <div className="app-left-column">
                <BookForm />
             </div>
-
             <div className="app-right-column">
                <Filter />
                <BookList />
--- 30-react-redux/frontend/src/components/Fitler/Filter.js
+++ 30-react-redux/frontend/src/components/Fitler/Filter.js
@@ -1,11 +1,22 @@
+import { useDispatch } from 'react-redux';
 import { setTitleFilter } from '../../redux/slices/filterSlice';
 import './Filter.css';
 
 const Filter = () => {
+   const dispatch = useDispatch();
+
+   const handleTitleFilterChange = (e) => {
+      dispatch(setTitleFilter(e.target.value));
+   };
+
    return (
       <div className="app-block filter">
          <div className="filter-group">
-            <input type="text" placeholder="Filter by title..." />
+            <input
+               type="text"
+               placeholder="Filter by title..."
+               onChange={handleTitleFilterChange}
+            />
          </div>
       </div>
    );
