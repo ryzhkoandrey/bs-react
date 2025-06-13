07cc19217f7277f3c057bedd69371c713891b38e
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-06-13 12:36:12 +0300

    30 - Filter by author

--- 30-react-redux/frontend/src/components/BookList/BookList.js
+++ 30-react-redux/frontend/src/components/BookList/BookList.js
@@ -37,7 +37,7 @@ const BookList = () => {
       <div className="app-block book-list">
          <h2>Book List</h2>
 
-         {books.length === 0 ? (
+         {filteredBooks.length === 0 ? (
             <p>No books available</p>
          ) : (
             <ul>
