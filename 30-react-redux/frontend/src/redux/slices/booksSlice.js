d137e7e437624806401782df7435794e1588afc6
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-06-15 11:24:29 +0300

    30 - Added thunkFunction

--- 30-react-redux/frontend/src/redux/slices/booksSlice.js
+++ 30-react-redux/frontend/src/redux/slices/booksSlice.js
@@ -1,4 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+import createBookWithID from '../../utils/createBookWithID';
 
 const initialState = [];
 
@@ -22,6 +24,18 @@ const booksSlice = createSlice({
       },
    },
 });
+
+export const thunkFunction = async (dispatch, getState) => {
+   try {
+      const res = await axios.get('http://localhost:4000/random-book');
+
+      if (res?.data?.title && res?.data?.author) {
+         dispatch(addBook(createBookWithID(res.data, 'API')));
+      }
+   } catch (error) {
+      console.log('Error fetching random book', error);
+   }
+};
 
 export const { addBook, deleteBook, toggleFavorite } = booksSlice.actions;
 
